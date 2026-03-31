6368035c9d68e2b7ffee9ce2c6d1981018618f78
Abdelilah Tehami <abdelilah.tehami@ump.ac.ma>
2026-03-31 20:10:57 +0100

    change python again

--- app.py
+++ app.py
@@ -788,4 +788,4 @@ def statistiques():
 if __name__ == '__main__':
     init_db()
     port = int(os.environ.get("PORT", 8000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=False)
